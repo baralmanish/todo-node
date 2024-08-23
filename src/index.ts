57b8d196462c598c1483311783924bc91a79de7c
Manish Baral <mail.aarman@gmail.com>
2024-08-23 23:12:12 +0400

    added body-parser and express-session

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
-// Import the 'express' module along with 'Request' and 'Response' types from express
 import express, { Request, Response } from "express";
-
-// Create an Express application
-const app = express();
+import session from "express-session";
+import bodyParser from "body-parser";
 
 // Specify the port number for the server
 const port: number = 3001;
 
-// Define a route for the root path ('/')
-app.get("/", (req: Request, res: Response) => {
-  // Send a response to the client
-  res.send("Hello, TypeScript + Node.js + Express!");
-});
+const startServer = async () => {
+  try {
+    // Create an Express application
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(
+      session({
+        secret: "your-secret-key",
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false } // Set secure to true in production
+      })
+    );
 
-// Start the server and listen on the specified port
-app.listen(port, () => {
-  // Log a message when the server is successfully running
-  console.log(`Server is running on http://localhost:${port}`);
-});
+    // Define a route for the root path ('/')
+    app.get("/", (req: Request, res: Response) => {
+      // Send a response to the client
+      res.send("Hello, TypeScript + Node.js + Express!");
+    });
+
+    // Start the server and listen on the specified port
+    app.listen(port, () => {
+      // Log a message when the server is successfully running
+      console.log(`API Server is running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error("API Server failed to start", error);
+  }
+};
+
+startServer();
